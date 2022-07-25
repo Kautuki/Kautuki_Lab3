222822fae75941d82ba64c217541a40318cdd0c7
Kautuki <80846505+Kautuki@users.noreply.github.com>
2022-07-25 14:18:52 +0530

    Add files via upload

---
+++ BalancedParenthesis.java
@@ -0,0 +1,45 @@
+package gldatastruct;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class BalancedParenthesis {
+    public
+    static boolean balancedParenthesis(String str) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            char x = str.charAt(i);
+            if (x == '(' || x == '[' || x == '{') {
+                stack.push(x);
+                continue;
+            }
+            if (stack.isEmpty()) return false;
+            char check;
+            switch (x) {
+                case ')':
+                    check = stack.pop();
+                    if (check == '{' || check == '[') return false;
+                    break;
+                case '}':
+                    check = stack.pop();
+                    if (check == '(' || check == '[') return false;
+                    break;
+                case ']':
+                    check = stack.pop();
+                    if (check == '(' || check == '{') return false;
+                    break;
+            }
+        }
+        return (stack.isEmpty());
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter input string");
+    	String str = sc.next();
+        if (balancedParenthesis(str))
+            System.out.println("True");
+        else
+            System.out.println("False");
+    }
+}
+
